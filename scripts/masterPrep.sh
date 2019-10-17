b5440c24e4745b33833fcd6a3eae6534ceba9e7e
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-10-17 21:27:23 +0300

    Update masterPrep.sh

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -30,7 +30,8 @@ echo $(date) " - System updates successfully installed"
 if hostname -f|grep -- "-0" >/dev/null
 then
     echo $(date) " - Installing Ansible, pyOpenSSL and python-passlib"
-    yum -y --enablerepo=epel install pyOpenSSL python-passlib
+    yum -y --enablerepo=epel install pyOpenSSL python-passlib python-pip
+    yes | pip install mitogen
     #yum -y install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.2-1.el7.ans.noarch.rpm
     yum -y install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.7.9-1.el7.ans.noarch.rpm
 	
